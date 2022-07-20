77fa44b0b1342987473cb48164953614aa318129
gakes <ttgakes@gmail.com>
2022-07-20 11:59:25 +0800

    123

---
+++ dx.java
@@ -0,0 +1,17 @@
+和包支付APP9.11.214登录：
+https://mca.cmpay.com:28710/mca/base/v1/init/dynamic-keys
+deviceSign:RSA加签
+x-lemon-sign：MD5
+密码登录：
+https://mca.cmpay.com:28710/security/login/password/dynamicKey
+password：HisunPKEncrypt加密
+
+电信APP9.4.0登录：
+发送验证码：
+https://appgologin.189.cn:9031/login/client/getLoginRandomCode
+
+验证码登录：
+https://appgologin.189.cn:9031/login/client/userLoginNormal
+loginAuthCipherAsymmertric：RSA加密
+
+有需要可以+V：bgy_era   
